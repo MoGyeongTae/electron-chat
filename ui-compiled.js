8d8b5f731f81af748cedf8a2a2e3fedf8cef6256
MoGyeongTae <alska2586@naver.com>
2018-09-10 11:54:57 +0900

    마지막업로드
    ㅇㅇ..

--- ui-compiled.js
+++ ui-compiled.js
@@ -10,7 +10,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var socket = io.connect("http://localhost:3000");
+var socket = io.connect("http://electronchat.gyeongtae.com");
 var currentWindow = remote.getCurrentWindow();
 
 // Title Component
@@ -41,7 +41,8 @@ var Title = function (_React$Component) {
 
     _this.state = {
       conusers: "",
-      nextchannel: "1"
+      nextchannel: "1",
+      channelList: [{ channel: "1", id: null }, { channel: "2", id: null }]
     };
     return _this;
   }
@@ -49,6 +50,13 @@ var Title = function (_React$Component) {
   _createClass(Title, [{
     key: "render",
     value: function render() {
+      var channels = this.state.channelList.map(function (item, key) {
+        return React.createElement(
+          "option",
+          { value: item.channel },
+          item.channel
+        );
+      });
       return React.createElement(
         "div",
         { className: "title" },
@@ -62,16 +70,7 @@ var Title = function (_React$Component) {
         React.createElement(
           "select",
           { className: "selectChannel", onChange: this.selectChange },
-          React.createElement(
-            "option",
-            { value: "1" },
-            "1\uCC44\uB110"
-          ),
-          React.createElement(
-            "option",
-            { value: "2" },
-            "2\uCC44\uB110"
-          )
+          channels
         ),
         React.createElement(
           "button",
@@ -160,23 +159,35 @@ var Chat = function Chat(props) {
     props.my != null && props.my && React.createElement(
       "div",
       { className: "myChat" },
-      props.username,
-      " : ",
-      props.message
+      React.createElement(
+        "p",
+        null,
+        props.username,
+        " : ",
+        props.message
+      )
     ),
     props.my != null && !props.my && React.createElement(
       "div",
       { className: "otherChat" },
-      props.username,
-      " : ",
-      props.message
+      React.createElement(
+        "p",
+        null,
+        props.username,
+        " : ",
+        props.message
+      )
     ),
     props.system && React.createElement(
       "div",
       { className: "systemChat" },
-      props.username,
-      " : ",
-      props.message
+      React.createElement(
+        "p",
+        null,
+        props.username,
+        " : ",
+        props.message
+      )
     )
   );
 };
@@ -214,7 +225,40 @@ var ImageChat = function ImageChat(props) {
       React.createElement(
         "p",
         null,
-        React.createElement("img", { src: props.url, width: "400", height: "auto" })
+        React.createElement(
+          "a",
+          { href: props.url, download: true },
+          React.createElement("img", { src: props.url, width: "400", height: "auto" })
+        )
+      )
+    )
+  );
+};
+
+var WhisperChat = function WhisperChat(props) {
+  return React.createElement(
+    "div",
+    null,
+    props.my && React.createElement(
+      "div",
+      { className: "myWhisperChat" },
+      React.createElement(
+        "p",
+        { style: { color: "white" } },
+        props.username,
+        " \uB2D8\uC5D0\uAC8C \uADD3\uC18D\uB9D0 : ",
+        props.message
+      )
+    ),
+    !props.my && React.createElement(
+      "div",
+      { className: "otherWhisperChat" },
+      React.createElement(
+        "p",
+        { style: { color: "white" } },
+        props.username,
+        " \uB2D8\uC73C\uB85C \uBD80\uD130 \uADD3\uC18D\uB9D0 : ",
+        props.message
       )
     )
   );
@@ -283,7 +327,14 @@ var ChatBox = function (_React$Component3) {
             chatLog: [].concat(_toConsumableArray(prevState.chatLog), [{ username: "System", message: data.message, my: null, system: true }])
           };
         });
-        _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight;
+        if (_this5.myChatBox.current.scrollTop < _this5.state.currentScroll) {
+          return;
+        } else {
+          _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight;
+        }
+        _this5.setState({
+          currentScroll: _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight
+        });
       });
 
       socket.on("system:leave", function (data) {
@@ -292,7 +343,14 @@ var ChatBox = function (_React$Component3) {
             chatLog: [].concat(_toConsumableArray(prevState.chatLog), [{ username: "System", message: data.message, my: null, system: true }])
           };
         });
-        _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight;
+        if (_this5.myChatBox.current.scrollTop < _this5.state.currentScroll) {
+          return;
+        } else {
+          _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight;
+        }
+        _this5.setState({
+          currentScroll: _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight
+        });
       });
 
       socket.on("receiveImage", function (data) {
@@ -302,7 +360,32 @@ var ChatBox = function (_React$Component3) {
             chatLog: [].concat(_toConsumableArray(prevState.chatLog), [{ username: data.username, message: null, url: data.url, my: ismy }])
           };
         });
-        _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight;
+        if (_this5.myChatBox.current.scrollTop < _this5.state.currentScroll) {
+          return;
+        } else {
+          _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight;
+        }
+        _this5.setState({
+          currentScroll: _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight
+        });
+      });
+
+      socket.on("receiveWhisper", function (data) {
+        var ismy = data.username == _this5.props.username;
+        console.log(data);
+        _this5.setState(function (prevState) {
+          return {
+            chatLog: [].concat(_toConsumableArray(prevState.chatLog), [{ username: data.username, message: data.message, whisper: true, my: ismy }])
+          };
+        });
+        if (_this5.myChatBox.current.scrollTop < _this5.state.currentScroll) {
+          return;
+        } else {
+          _this5.myChatBox.current.scrollTop = _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight;
+        }
+        _this5.setState({
+          currentScroll: _this5.myChatBox.current.scrollHeight - _this5.myChatBox.current.offsetHeight
+        });
       });
     }
   }, {
@@ -311,16 +394,21 @@ var ChatBox = function (_React$Component3) {
       var chatLog = this.state.chatLog;
 
       var chat = chatLog.map(function (item, idx) {
-        if (item.message != null) return React.createElement(Chat, {
+        if (!item.whisper && !item.url) return React.createElement(Chat, {
           username: item.username,
           message: item.message,
           my: item.my,
           system: item.system,
           key: idx
-        });else return React.createElement(ImageChat, {
+        });else if (item.url) return React.createElement(ImageChat, {
           username: item.username,
           my: item.my,
           url: item.url,
+          key: idx
+        });else if (item.whisper) return React.createElement(WhisperChat, {
+          username: item.username,
+          message: item.message,
+          my: item.my,
           key: idx
         });
       });
@@ -341,6 +429,7 @@ var ChatBox = function (_React$Component3) {
 }(React.Component);
 
 // ChatInput Component
+// props - username , channel
 
 
 var ChatInput = function (_React$Component4) {
@@ -360,17 +449,26 @@ var ChatInput = function (_React$Component4) {
     };
 
     _this6.buttonClick = function () {
-      socket.emit("sendChat", { username: _this6.props.username, message: _this6.state.input, channel: _this6.props.channel });
-      _this6.setState(function (prevState, props) {
-        return { input: "" };
-      });
+      if (_this6.state.whisperUser) {
+        socket.emit("sendWhisper", { username: _this6.props.username, message: _this6.state.input, whisper: _this6.state.whisperUser });
+        _this6.setState(function (prevState, props) {
+          return { input: "" };
+        });
+      } else {
+        socket.emit("sendChat", { username: _this6.props.username, message: _this6.state.input, channel: _this6.props.channel });
+        _this6.setState(function (prevState, props) {
+          return { input: "" };
+        });
+      }
     };
 
     _this6.onFocus = function () {
+      if (_this6.state.whisperUser) return;
       socket.emit("startChat", { channel: _this6.props.channel });
     };
 
     _this6.onBlur = function () {
+      if (_this6.state.whisperUser) return;
       socket.emit("endChat", { channel: _this6.props.channel });
     };
 
@@ -383,9 +481,22 @@ var ChatInput = function (_React$Component4) {
     };
 
     _this6.fileChange = function (e) {
-      e.persist();
+      var file = _this6.fileInput.current.files;
+      _this6.fileLabel.current.innerText = file[0].name;
+    };
+
+    _this6.fileSend = function (e) {
+      if (_this6.state.whisper) {
+        return;
+      }
       var exArray = ["jpg", "png", "jpeg", "gif"];
-      var file = e.target.files;
+      var file = _this6.fileInput.current.files;
+      if (!file[0]) {
+        notifier.notify('파일이 없습니다', {
+          message: "\uD30C\uC77C\uC744 \uCD94\uAC00\uD574 \uC8FC\uC2E0 \uD6C4 \uB20C\uB7EC\uC8FC\uC138\uC694"
+        });
+        return;
+      }
       var type = file[0].type.split("/")[1];
       if (exArray.indexOf(type) < 0) {
         e.target.value = "";
@@ -396,32 +507,84 @@ var ChatInput = function (_React$Component4) {
       }
       var reader = new FileReader();
       reader.onload = function () {
-        _this6.fileSend(reader.result);
+        socket.emit("sendImage", { username: _this6.props.username, url: reader.result, channel: _this6.props.channel });
         e.target.value = "";
       };
       reader.readAsDataURL(file[0]);
+      _this6.fileLabel.current.innerText = "이미지 선택";
     };
 
-    _this6.fileSend = function (result) {
-      socket.emit("sendImage", { username: _this6.props.username, url: result, channel: _this6.props.channel });
+    _this6.selectChange = function (e) {
+      var value = e.target.value;
+
+      console.log(value);
+      _this6.setState({ whisperUser: value });
     };
 
     _this6.state = {
-      input: ""
+      input: "",
+      whisperUser: "",
+      conuserList: []
     };
+    _this6.fileInput = React.createRef();
+    _this6.fileLabel = React.createRef();
     return _this6;
   }
 
   _createClass(ChatInput, [{
+    key: "componentWillReceiveProps",
+    value: function componentWillReceiveProps(nextProps) {
+      if (JSON.stringify(this.props.channel) !== JSON.stringify(nextProps.channel)) // Check if it's a new user, you can also use some unique, like the ID
+        {
+          this.setState({
+            whisperUser: ""
+          });
+        }
+    }
+  }, {
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this7 = this;
+
+      socket.on("conusers", function (data) {
+        _this7.setState({
+          conuserList: data.userlist
+        });
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this8 = this;
+
+      var conuserList = this.state.conuserList;
+
+      var list = conuserList.map(function (item, key) {
+        if (item.username == _this8.props.username) {
+          return React.createElement(
+            "option",
+            { value: "" },
+            "\uC5C6\uC74C"
+          );
+        }
+        return React.createElement(
+          "option",
+          { value: item.id },
+          item.username
+        );
+      });
       return React.createElement(
         "div",
         { className: "functionBox" },
         React.createElement(
           "div",
           { className: "inputBox" },
-          React.createElement("input", { type: "text", style: { width: "90%", height: "30px", float: "left" }, value: this.state.input, onChange: this.onChange, onFocus: this.onFocus, onBlur: this.onBlur, onKeyPress: this.onKeyPress }),
+          React.createElement(
+            "select",
+            { onChange: this.selectChange, value: this.state.whisperUser },
+            list
+          ),
+          React.createElement("input", { type: "text", value: this.state.input, onChange: this.onChange, onFocus: this.onFocus, onBlur: this.onBlur, onKeyPress: this.onKeyPress }),
           React.createElement(
             "button",
             { onClick: this.buttonClick, className: "btnSend" },
@@ -433,10 +596,15 @@ var ChatInput = function (_React$Component4) {
           { className: "filebox" },
           React.createElement(
             "label",
-            { "for": "fileupload" },
-            "\uC774\uBBF8\uC9C0 \uC5C5\uB85C\uB4DC"
+            { "for": "fileupload", ref: this.fileLabel },
+            "\uC774\uBBF8\uC9C0 \uC120\uD0DD"
           ),
-          React.createElement("input", { type: "file", id: "fileupload", onChange: this.fileChange })
+          React.createElement("input", { type: "file", id: "fileupload", ref: this.fileInput, onChange: this.fileChange }),
+          React.createElement(
+            "button",
+            { className: "btn", onClick: this.fileSend },
+            "\uC774\uBBF8\uC9C0 \uC804\uC1A1"
+          )
         )
       );
     }
@@ -454,15 +622,15 @@ var App = function (_React$Component5) {
   function App(props) {
     _classCallCheck(this, App);
 
-    var _this7 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+    var _this9 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    _this7.askNick = function () {
-      if (_this7.state.username) {
+    _this9.askNick = function () {
+      if (_this9.state.username) {
         return;
       }
       prompt({
-        title: 'Insert Your Name',
-        label: 'Insert Your NickName In ChatApp Plz',
+        title: '이름을 입력하세요',
+        label: '당신이 채팅앱에서 쓸 이름을 입력해주세요',
         value: '',
         inputAttrs: { // attrs to be set if using 'input'
           type: 'text'
@@ -470,38 +638,38 @@ var App = function (_React$Component5) {
         type: 'input' // 'select' or 'input, defaults to 'input'
       }).then(function (r) {
         if (!r) {
-          _this7.askNick();
+          _this9.askNick();
           return;
         }
-        _this7.setState({
+        _this9.setState({
           username: r
         });
         socket.emit("initUser", { username: r });
         socket.on("overlap", function (data) {
-          _this7.setState({
+          _this9.setState({
             username: ""
           });
-          _this7.askNick();
+          _this9.askNick();
           return;
         });
         return;
       }).catch(console.error);
     };
 
-    _this7.state = {
+    _this9.state = {
       username: "",
       channel: "1"
     };
-    return _this7;
+    return _this9;
   }
 
   _createClass(App, [{
     key: "componentDidMount",
     value: function componentDidMount() {
-      var _this8 = this;
+      var _this10 = this;
 
-      axios.get("http://download.gyeongtae.com/chatver?version=0.03").then(function (data) {
-        if (data.data.result == 0) {
+      axios.get("http://download.gyeongtae.com/chatver?version=0.1").then(function (data) {
+        if (!data.data.result) {
           var obj = {
             title: '새로운 버전이 있습니다',
             body: '채팅앱을 업데이트 하려면 클릭하세요'
@@ -511,7 +679,7 @@ var App = function (_React$Component5) {
             shell.openExternal("http://download.gyeongtae.com/chat");
           };
         }
-        _this8.askNick();
+        _this10.askNick();
       }).catch(function (err) {
         console.log(err);
       });
@@ -521,7 +689,7 @@ var App = function (_React$Component5) {
         });
       });
       socket.on("changeChannel", function (data) {
-        _this8.setState({
+        _this10.setState({
           channel: data.channel
         });
       });
